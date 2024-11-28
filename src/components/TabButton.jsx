d07bfff8a11c67958edb6c2454693d7cc07db888
Giridhar Kellampalli <giridhar_kellampalli@optum.com>
2024-11-27 19:11:00 -0600

    Forward Props to Wrapper Elements

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,9 +1,11 @@
-export default function TabButton({children, onSelect, isSelected}) {
+export default function TabButton({children, isSelected, ...props}) {
 
 
-    return (<li>
-            <button className={isSelected ? 'active' : undefined} onClick={onSelect}>
-                {children}
+    return (
+        <li>
+            <button className={ isSelected ? 'active' : undefined } {...props}>
+                { children }
             </button>
-        </li>)
+        </li>
+    )
 }
